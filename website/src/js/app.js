c3c607466eb149a929fc503ab9160fb1f4fdb011
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-03-08 21:54:56 +0000

    add firebase

--- website/src/js/app.js
+++ website/src/js/app.js
@@ -1,60 +1,64 @@
 // External
-import 'airbnb-browser-shims';
+import 'airbnb-browser-shims'
 
 // Internal
-import './modules/helpers';
+import './modules/helpers'
+import(/* webpackPreload: true */ './modules/forcast')
 
-const body = document.getElementsByTagName('body')[0];
-const html = document.getElementsByTagName('html')[0];
+const body = document.getElementsByTagName('body')[0]
+const html = document.getElementsByTagName('html')[0]
 
 // Remove loading class fron bosy on window load.
 window.onload = () => {
-	window.setTimeout(() => {
-		body.classList.remove('is-loading');
-	}, 100);
-};
+  window.setTimeout(() => {
+    body.classList.remove('is-loading')
+  }, 100)
+}
 
-html.setAttribute('data-browser', browser.name);
+html.setAttribute('data-browser', browser.name)
 
 // LocalStroage.
 if (typeof localStorage !== 'undefined') {
-	if (localStorage.getItem('theme')) {
-		const theme = localStorage.getItem('theme');
-		body.removeAttribute('data-theme', 'light');
-		body.removeAttribute('data-theme', 'dark');
-		body.setAttribute('data-theme', theme);
-	}
+  if (localStorage.getItem('theme')) {
+    const theme = localStorage.getItem('theme')
+    body.removeAttribute('data-theme', 'light')
+    body.removeAttribute('data-theme', 'dark')
+    body.setAttribute('data-theme', theme)
+  }
 }
 
 // set dark mode if user's system prefers it.
 if (window.matchMedia) {
-	if (process.env.NODE_ENV !== 'production') {
-		console.log('supports matchmedia');
-	}
-	if (matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') === null) {
-		if (process.env.NODE_ENV !== 'production') {
-			console.log('prefers dark');
-		}
-		body.removeAttribute('data-theme', 'light');
-		body.setAttribute('data-theme', 'dark');
-	}
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('supports matchmedia')
+  }
+  if (
+    matchMedia('(prefers-color-scheme: dark)').matches &&
+    localStorage.getItem('theme') === null
+  ) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('prefers dark')
+    }
+    body.removeAttribute('data-theme', 'light')
+    body.setAttribute('data-theme', 'dark')
+  }
 }
 
 // Switch between dark and light mode.
-const switchTheme = document.getElementById('switch-theme');
+const switchTheme = document.getElementById('switch-theme')
 
-switchTheme.addEventListener('click', (e) => {
-	e.preventDefault();
+switchTheme.addEventListener('click', e => {
+  e.preventDefault()
 
-	body.classList.add('color-theme-in-transition');
-	if (body.getAttribute('data-theme') === 'light') {
-		body.setAttribute('data-theme', 'dark');
-		localStorage.setItem('theme', 'dark');
-	} else {
-		body.setAttribute('data-theme', 'light');
-		localStorage.setItem('theme', 'light');
-	}
-	window.setTimeout(() => {
-		body.classList.remove('color-theme-in-transition');
-	}, 1500);
-});
+  body.classList.add('color-theme-in-transition')
+  if (body.getAttribute('data-theme') === 'light') {
+    body.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+  } else {
+    body.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
+  }
+  window.setTimeout(() => {
+    body.classList.remove('color-theme-in-transition')
+  }, 1500)
+})
