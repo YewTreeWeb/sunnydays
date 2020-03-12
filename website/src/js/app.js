8d49c92b02925399ef36cd5296dcf1bae0075234
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-03-12 22:03:24 +0000

    getting api and adding data to dom

--- website/src/js/app.js
+++ website/src/js/app.js
@@ -1,16 +1,15 @@
 // External
 import 'airbnb-browser-shims'
+import localForage from 'localforage'
 
 // Internal
 import './modules/helpers'
-import(
-  /* webpackPreload: true */ /* webpackChunkName: "forcast" */ './modules/forcast'
-)
+import { getCity, getWeather } from './modules/forcast'
 
 const body = document.getElementsByTagName('body')[0]
 const html = document.getElementsByTagName('html')[0]
 
-// Remove loading class fron bosy on window load.
+// Remove loading class from body on window load.
 window.onload = () => {
   window.setTimeout(() => {
     body.classList.remove('is-loading')
@@ -19,7 +18,7 @@ window.onload = () => {
 
 html.setAttribute('data-browser', browser.name)
 
-// LocalStroage.
+// LocalStorage.
 if (typeof localStorage !== 'undefined') {
   if (localStorage.getItem('theme')) {
     const theme = localStorage.getItem('theme')
@@ -63,4 +62,59 @@ switchTheme.addEventListener('click', e => {
   window.setTimeout(() => {
     body.classList.remove('color-theme-in-transition')
   }, 1500)
+})
+
+// Getting weather
+const cityForm = document.querySelector('form')
+const card = document.querySelector('.card')
+const content = document.querySelector('.media-content')
+
+// Output all data to DOM.
+const updateUI = data => {
+  // destructure properties
+  const { cityData, weather } = data
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(cityData)
+    console.log(weather)
+  }
+
+  // update template
+  content.innerHTML = `
+    <div class="media-content">
+      <p class="title is-4">${cityData.EnglishName}</p>
+      <p class="subtitle is-6">${weather.WeatherText}</p>
+      <div class="content">
+        <p>${weather.Temperature.Metric.Value} <span>&deg;C</span></p>
+        <br>
+        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      </div>
+    </div>
+  `
+
+  // Show weather card if card doesn't have hidden class
+  if (card.classList.contains('is-hidden')) {
+    card.classList.remove('is-hidden')
+  }
+}
+
+const updateCity = async city => {
+  const cityData = await getCity(city)
+  const weather = await getWeather(cityData.Key)
+
+  return { cityData, weather }
+}
+
+cityForm.addEventListener('submit', e => {
+  // prevent the default submit.
+  e.preventDefault()
+
+  // get city value.
+  const city = cityForm.city.value.trim()
+  cityForm.reset()
+
+  // update ui with city
+  updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.error(err))
 })
--- website/src/js/modules/forcast.js
+++ website/src/js/modules/forcast.js
@@ -1,1 +1,24 @@
-console.log('Forcast API')
+// Add API key from AccuWeather.
+const key = 'GH0k8lZNzQiXuGPW4SVCwJt4Fl8RXyOl'
+
+// Get Weather
+export const getWeather = async location => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+  const query = `${location}?apikey=${key}`
+
+  const response = await fetch(base + query)
+  const data = await response.json()
+
+  return data[0]
+}
+
+// Get city info
+export const getCity = async city => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+  const query = `?apikey=${key}&q=${city}`
+
+  const response = await fetch(base + query)
+  const data = await response.json()
+
+  return data[0]
+}
